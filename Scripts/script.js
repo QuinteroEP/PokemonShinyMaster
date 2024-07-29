a205ebc9b90e21e82cd19d643a558ceb84aa6b2a
Pablo Enrique Quintero Callamand <140966677+QuinteroEP@users.noreply.github.com>
2024-07-29 12:50:10 -0500

    V2.0
    90% completado
    
    Falta:
    -Actualizar correctamente la lista con la informacion del Pokemon
    -Subir y mostrar imagen del Pokemon

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,13 +1,15 @@
 window.onload = () => {
-    /*Datos de prueba*/
-    let caught = [];
-    create("Shine", "X", "1", "Random", "No", "a")
-    for (let i = 0; i<caught.length; i++)
-        {
-            tableBuilder(caught[i])
-        }
-
     console.log("Script loaded");
+    
+    /*Datos de prueba*/
+
+    let caught = [];
+    function create(name, species, game, Encounters, method, charm, url)
+    {
+        let Shiny = new Pokemon(name, species, game, Encounters, method, charm, url)
+        caught.push(Shiny);
+    }
+
     /*Objeto Pokemon*/
     let Shiny = {
         Nickname: "Doe",
@@ -15,7 +17,7 @@ window.onload = () => {
         originGame: "Red",
         Encounters: "123",
         Method: "Random Encounters",
-        Charm: "no",
+        Charm: "No",
         Photo: ""
     }
 
@@ -28,14 +30,18 @@ window.onload = () => {
         this.Charm = charm
         this.Photo = pic
     }
+    //Eliminar tabla base
+    const body = document.getElementById("tableBody")
+    body.deleteRow(0);
 
-    function create(name, species, game, Encounters, method, charm, url)
-    {
-        let Shiny = new Pokemon(name, species, game, Encounters, method, charm, url)
-        caught.push(Shiny);
-    }
+    //Construir tabla inicial
+    create("Jane Doe", "Mew", "Red", "123", "Random", "No", "")
+    create("Shine", "Onyx", "X", "0","Random", "No", "")
 
-    /*Construir tabla inicial*/
+    for (let i = 0; i<caught.length; i++)
+        {
+            tableBuilder(caught[i])
+        }
 
     /*obtener variables del formulario*/
 
@@ -51,40 +57,41 @@ window.onload = () => {
     /*Encounters*/
     const lstM = document.getElementById("lstMethod");
     /*Method*/
-    const chOp = document.getElementsById('charmOption')
+    const chOp = document.getElementById('charmCheck')
     /*Charm?*/
     const pic = document.getElementById("photo");
     /*Photo URL*/
-   
+    var listExists = false;
 
     form.addEventListener("submit", (event) => {
         event.preventDefault();
 
-        console.log(chOp.value)
+        console.log(chOp);
+        getCharmOpt(chOp);
+
+        /*Vaciar la tabla antes de actualizarla*/
+        var index = caught.length - 1;
+        clearTable(index);
 
         /*Construir nuevo objeto*/
         create(txtN.value, txtP.value, lstG.value, nEnc.value, lstM.value, chOp, pic.value);
         console.log(caught)
 
-        /*Modificar tabla*/
+        /*Actualizar tabla*/
         for (let i = 0; i<caught.length; i++)
         {
             tableBuilder(caught[i])
         }
-
-
     })
 
     function tableBuilder(Shiny){
-        if(Shiny.Nickname == null)
+        if(Shiny.Nickname == "")
             {
                 rowName = Shiny.Species
             }
             else{
                 rowName = Shiny.Nickname
             }
-
-        const body = document.getElementById("tableBody")
 
         /*Nueva fila*/
         const newRow = document.createElement("tr")
@@ -97,13 +104,93 @@ window.onload = () => {
         /*Agregar boton*/
         const infoCell = document.createElement("td")
         const infoButton = document.createElement("button")
+        infoButton.id = rowName + "Button"
         infoButton.textContent = "Show Info"
         infoCell.appendChild(infoButton)
         newRow.appendChild(infoCell)
 
         body.appendChild(newRow)
+
+        infoButton.addEventListener("click", function(){
+            showInfo(Shiny);
+        })
+    }
+
+    function clearTable(index){
+     for(let i = 0; i<=index; i++){
+        body.deleteRow(0);
+     }
+     console.log("Deleted " + caught.length + " rows")
     }
 
     /*Mostrar informacion*/
+    function showInfo(pokemon){
+       
+        console.log("button for " + pokemon.Nickname)
 
+        console.log(pokemon)
+
+        var infoDiv = document.getElementById("information");
+
+        //Lista de informacion
+        var infoList = document.createElement("ul");
+        var item1 = document.createElement("li");
+        var item2 = document.createElement("li");
+        var item3 = document.createElement("li");
+        var item4 = document.createElement("li");
+        var item5 = document.createElement("li");
+        var item6 = document.createElement("li");
+
+        //actualizar tabla
+        clearList(infoList);
+
+        console.log("Creating New List")
+        item1.textContent = "Nickname: " + pokemon.Nickname;
+        infoList.appendChild(item1)
+
+        infoDiv.appendChild(infoList);
+
+        item2.textContent = "Species: " + pokemon.Species;
+        infoList.appendChild(item2)
+
+        infoDiv.appendChild(infoList);
+
+        item3.textContent = "Game: " + pokemon.originGame;
+        infoList.appendChild(item3)
+
+        infoDiv.appendChild(infoList);
+
+        item4.textContent = "Encounters: " + pokemon.Encounters;
+        infoList.appendChild(item4)
+
+        infoDiv.appendChild(infoList);
+
+        item5.textContent = "Method: " + pokemon.Method;
+        infoList.appendChild(item5)
+
+        infoDiv.appendChild(infoList);
+
+        item6.textContent = "Shiny charm?: " + pokemon.charm;
+        infoList.appendChild(item6)
+
+        infoDiv.appendChild(infoList);
+    }
+    
+    function getCharmOpt(radio){
+        console.log("checking for charm. ");
+
+        for(var i = 0; i<radio.length; i++){
+            if(radio[i].checked){
+                console.log(radio[i].value)
+            }   
+        }
+    }
+
+    function clearList(ul){
+        console.log("Clearing List")
+        while(ul.firstChild){
+            ul.removeChild(firstChild);
+        }
+        console.log("Done")
+    }
 }
